be5cae5c90ed019026dd2d2bebc27efbf37b735b
Nitansh <24b1807@iitb.ac.in>
2026-01-19 13:13:49 +0530

    .

--- week 2/week2.cpp
+++ week 2/week2.cpp
@@ -5,20 +5,20 @@
 #include <fstream>
 using namespace std;
 
-const int L = 100;                   // No. of lattice sites
-double hop_rate = 1.0;               // Hop rate
-double tumble_rate = 0.01;                  // Tumble rate
-double simulation_time = 100.0;     // Total simulation time
+const int L = 100; // length
+double gamma = 1.0; // rate of hopping
+double alpha = 0.01; // rate of tumbling
+double simulation_time = 100.0; // simul ends when
 
-mt19937 rng(42);                  // Random number generator with fixed seed
-uniform_real_distribution<double> uniform_dist(0.0, 1.0);
+mt19937 rng(42); // random number generation to help choose random time of next reaction
+uniform_real_distribution<double> uniform_dist(0.0, 1.0); // to choose what state occured
 
 struct Particle {
     int position;
     int orientation;
 };
 
-int periodic(int x) {
+int periodic(int x) { // boundary conditions
     while (x < 0) x += L;
     while (x >= L) x -= L;
     return x;
@@ -26,13 +26,10 @@ int periodic(int x) {
 
 int main() {
     Particle p1, p2;
-
-    p1.position = 40;
+    p1.position = 40; // ICS
     p1.orientation = 1;  // Right
-
     p2.position = 90;
     p2.orientation = -1; // Left
-
     double current_time = 0.0;
 
     ofstream traj_file("trajectory.dat");
@@ -43,21 +40,21 @@ int main() {
 
         int x1_next = periodic(p1.position + p1.orientation);
         if (x1_next != p2.position) {
-            rates.push_back(hop_rate);
-            events.push_back(1); // Particle 1 hops
+            rates.push_back(gamma);
+            events.push_back(1); 
         }
 
         int x2_next = periodic(p2.position + p2.orientation);
         if (x2_next != p1.position) {
-            rates.push_back(hop_rate);
-            events.push_back(2); // Particle 2 hops
+            rates.push_back(gamma);
+            events.push_back(2); 
         }
 
-        rates.push_back(tumble_rate);
-        events.push_back(3); // Particle 1 tumbles
+        rates.push_back(alpha);
+        events.push_back(3); 
 
-        rates.push_back(tumble_rate);
-        events.push_back(4); // Particle 2 tumbles
+        rates.push_back(alpha);
+        events.push_back(4); 
 
         double total_rate = 0.0;
         for (double r : rates) total_rate += r;
